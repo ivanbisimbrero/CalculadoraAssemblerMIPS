367d20964c069b0c782de3f28ec31edb377b1d7c
ivanbisimbrero <ivanbisimbrero@gmail.com>
2022-12-03 11:57:34 +0100

    ARREGLOS
    Arreglada la salida del menu que entraba en bucle y no salia. Funciones suma, resta, producto y division completamente funcionales y mostrando resultados por pantalla, fibonacci no muestra todavia resultados por pantalla.

--- CODIGO_ASSEMBLER/calculadora.s
+++ CODIGO_ASSEMBLER/calculadora.s
@@ -32,39 +32,36 @@ mensajeError: .asciiz "ERROR. DATO INTRODUCIDO NO VÁLIDO"
 .align 4
 comment:.asciiz "FIN DE PROGRAMA\n"
 .align 4
-	.text
-	.globl main
+    .text
+    .globl main
 main:
     # Cargamos los distintos valores para
-	# Prologo
-	subu $sp, $sp, 24 
-	sw $ra, 4($sp) 
-	sw $a0, 20($sp)
+    # Prologo
+    subu $sp, $sp, 24
 
     # Llamamos a la rutina menu para empezar a mostrar la pantalla del menú
-	jal menu
-	j fin
+    jal menu
 
-	# Epilogo
-	lw $ra, 4($sp) 
-	lw $a0, 20($sp) 
-	addu $sp, $sp, 24
+    # Epilogo
+    addu $sp, $sp, 24
+    j fin
 
-	
-menu:  
-	# Imprimimos por pantalla las opciones
-	la $a0 mensajeMenu
-	li $v0 4
+
+
+menu:
+    # Imprimimos por pantalla las opciones
+    la $a0 mensajeMenu
+    li $v0 4
     syscall
-    # Leemos el caracter introducido por el usuario
+    #Leemos el caracter introducido por el usuario
     la $a0 buffer
     li $a1 5
     li $v0 8
     syscall
-	lb $t7 buffer
+    lb $t7 buffer
     # Comparar el valor del caracter introducido por el usuario
     beq $t7 '.' end_Menu
-	beq $t7 'S' suma
+    beq $t7 'S' suma
     beq $t7 'R' resta
     beq $t7 'P' producto
     beq $t7 'D' division
@@ -93,146 +90,159 @@ read_float:
 
     #Funcion para que cargue los valores
 carga_valores:
-	#PROLOGO
-	subu $sp $sp 4
-	sw $ra ($sp)
+    #PROLOGO
+    subu $sp $sp 8
+    sw $ra ($sp)
 
-	jal read_int
-	jal read_float
+    jal read_int
+    jal read_float
 
-	#EPILOGO
-	lw $ra ($sp)
-	addu $sp $sp 4
+    #EPILOGO
+    lw $ra ($sp)
+    addu $sp $sp 8
 
-	# Leemos valores
-	lw $t0 numEntero
-	l.s $f0 numFloat
+    # Leemos valores
+    lw $t0 numEntero
+    l.s $f0 numFloat
 
-	# Pasamos el entero a float
-	mtc1 $t0 $f1
-	cvt.s.w $f1 $f1 
+    # Pasamos el entero a float
+    mtc1 $t0 $f1
+    cvt.s.w $f1 $f1
 
-	jr $ra 
+    jr $ra
 
 suma:
-	# Llamamos a la función que carga los valores
-	jal carga_valores
+    #Guardamos la direccion de retorno del menu
+    subu $sp $sp 8
+    sw $ra ($sp)
+    # Llamamos a la función que carga los valores
+    jal carga_valores
 
-	# Sumamos los dos floats
-	add.s $f2 $f0 $f1
+    # Sumamos los dos floats
+    add.s $f2 $f0 $f1
 
-	# Almacenamos el resultado en la dirección de memoria resultado 
-      s.s $f2 resultado
+    # Almacenamos el resultado en la dirección de memoria resultado
+    s.s $f2 resultado
 
-	# Imprimimos el resultado
-	j mostrar_resultado_float
+    # Imprimimos el resultado
+    j mostrar_resultado_float
 
 resta:
-	# Llamamos a la función que carga los valores
-	jal carga_valores
+    #Guardamos la direccion de retorno del menu
+    subu $sp $sp 8
+    sw $ra ($sp)
+    # Llamamos a la función que carga los valores
+    jal carga_valores
 
-	# Restamos los dos floats
-	sub.s $f2 $f2 $f0
+    # Restamos los dos floats
+    sub.s $f2 $f1 $f0
 
-	# Almacenamos el resultado
+    # Almacenamos el resultado
     mfc1 $t1 $f2
     sw $t1 resultado
 
-	# Imprimimos el resultado
-	j mostrar_resultado
+    # Imprimimos el resultado
+    j mostrar_resultado_float
 
 producto:
-	# Llamamos a la función que carga los valores
-	jal carga_valores
+    #Guardamos la direccion de retorno del menu
+    subu $sp $sp 8
+    sw $ra ($sp)
+    # Llamamos a la función que carga los valores
+    jal carga_valores
 
-	# Multiplicamos los dos floats
-	mul.s $f2 $f0 $f1
+    # Multiplicamos los dos floats
+    mul.s $f2 $f0 $f1
 
-	# Almacenamos el resultado en la dirección de memoria resultado
+    # Almacenamos el resultado en la dirección de memoria resultado
     mfc1 $t1 $f2
     sw $t1 resultado
 
-	# Imprimimos el resultado
-	j mostrar_resultado
+    # Imprimimos el resultado
+    j mostrar_resultado_float
 
 division:
-	# Llamamos a la función que carga los valores
-	jal carga_valores
+    #Guardamos la direccion de retorno del menu
+    subu $sp $sp 8
+    sw $ra ($sp)
+    # Llamamos a la función que carga los valores
+    jal carga_valores
 
-	# Dividimos los dos floats
-	div.s $f2 $f2 $f0
+    # Dividimos los dos floats
+    div.s $f2 $f1 $f0
 
-	# Almacenamos el resultado
+    # Almacenamos el resultado
     mfc1 $t1 $f2
     sw $t1 resultado
 
-	# Imprimimos el resultado
-	j mostrar_resultado
+    # Imprimimos el resultado
+    j mostrar_resultado_float
 
 # INICIO DE FIBONACCI
 fibonacci:
 
-# Inicio del prólogo
-subu $sp, $sp, 24
-sw $ra, 20($sp)
-sw $s0, 16($sp)
-sw $s1, 12($sp)
-# Fin del prólogo
+    # Inicio del prólogo
+    subu $sp, $sp, 24
+    sw $ra, 20($sp)
+    sw $s0, 16($sp)
+    sw $s1, 12($sp)
+    # Fin del prólogo
 
-move $s0, $a0 # movemos el dato introducido por el usuario al registro $s0 o el valor de la primera llamada recursiva
-li $v0, 1 # cargamos un 1 en $v0 como resultado en los casos que n=1, n=2
-ble $s0, 2, fibonacciFin # si los valores introducidos son n=1 o n=2 entonces se salta al final de fibonacci
-subu $a0, $s0, 1 # primer argumento en la suma de la secuencia de fibonacci (n-1), siendo n=$s0
-jal fibonacci # hacemos una llamada recursiva a fibonacci hasta que $a0=1 ó $a0=2
-move $s1, $v0 # almacenamos el valor de $v0 en $s1
-subu $a0, $s0, 2 # segundo argumento en la suma de la secuencia de fibonacci (n-1), siendo n=$s0
-jal fibonacci # hacemos una llamada recursiva a fibonacci hasta que $a0=1 ó $a0=2
-add $v0, $s1, $v0 # sumamos los valores de fibonacci(n-1) y fibonacci(n-2)
-
-# Fin de la secuencia de Fibonacci
+    move $s0, $a0 # movemos el dato introducido por el usuario al registro $s0 o el valor de la primera llamada recursiva
+    li $v0, 1 # cargamos un 1 en $v0 como resultado en los casos que n=1, n=2
+    ble $s0, 2, fibonacciFin # si los valores introducidos son n=1 o n=2 entonces se salta al final de fibonacci
+    subu $a0, $s0, 1 # primer argumento en la suma de la secuencia de fibonacci (n-1), siendo n=$s0
+    jal fibonacci # hacemos una llamada recursiva a fibonacci hasta que $a0=1 ó $a0=2
+    move $s1, $v0 # almacenamos el valor de $v0 en $s1
+    subu $a0, $s0, 2 # segundo argumento en la suma de la secuencia de fibonacci (n-1), siendo n=$s0
+    jal fibonacci # hacemos una llamada recursiva a fibonacci hasta que $a0=1 ó $a0=2
+    add $v0, $s1, $v0 # sumamos los valores de fibonacci(n-1) y fibonacci(n-2)
+    # Fin de la secuencia de Fibonacci
 fibonacciFin:
 
-# Inicio del epílogo
-lw $ra, 20($sp)
-lw $s0, 16($sp)
-lw $s1, 12($sp)
-addu $sp, $sp, 24
-jr $ra
-# Fin del epílogo
+    # Inicio del epílogo
+    lw $ra, 20($sp)
+    lw $s0, 16($sp)
+    lw $s1, 12($sp)
+    addu $sp, $sp, 24
+    j menu
+    # Fin del epílogo
 
 # FIN DE FIBONACCI
 
 mostrar_error:
-	la $a0 mensajeError
-	li $v0 4
-	syscall
-	j menu
+la $a0 mensajeError
+li $v0 4
+syscall
+j menu
 
 mostrar_resultado_int:
-    la $a0 mensajeResultado
-    li $v0 4
-    syscall
-    lw $a0 resultado
-    li $v0 1
-    syscall
-    j menu
+la $a0 mensajeResultado
+li $v0 4
+syscall
+lw $a0 resultado
+li $v0 1
+syscall
+j menu
 
 mostrar_resultado_float:
-	la $a0 mensajeResultado
-	li $v0 4
-	syscall
-	l.s $f12 resultado
-	li $v0 2
-	syscall
-	j menu
+la $a0 mensajeResultado
+li $v0 4
+syscall
+l.s $f12 resultado
+li $v0 2
+syscall
+lw $ra ($sp)
+addu $sp $sp 8
+j menu
 
 end_Menu:
-    #Mostramos el mensaje de fin de programa
-    la $a0 comment
-    li $v0 4
-    syscall
-	jr $ra     #Salimos de menu y volvemos al main 
-                     
-fin:	
-	li $v0 10
-	syscall
+#Mostramos el mensaje de fin de programa
+la $a0 comment
+li $v0 4
+syscall
+jr $ra     #Salimos de menu y volvemos al main
+                 
+fin:
+li $v0 10
+syscall
